6edd3d000dfbdd1b54af42fcf96573b944f31c00
jenny <jennykim6890@naver.com>
2023-03-03 19:49:36 +0900

    세팅커밋

---
+++ src/main/java/com/example/airb/AirbApplication.java
@@ -0,0 +1,13 @@
+package com.example.airb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbApplication.class, args);
+	}
+
+}
